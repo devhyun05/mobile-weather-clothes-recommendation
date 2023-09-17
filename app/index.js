6600d256049c3c63af758ac296aa9b96445f69ed
HyunSeongLee <devhyun05@gmail.com>
2023-09-17 01:19:13 -0400

    added loading screen and data waits until loads successfully done

--- app/index.js
+++ app/index.js
@@ -1,77 +1,96 @@
 import { useState, useEffect } from 'react';
-import { TextInput, Button, View, Text, StyleSheet } from 'react-native'; 
+import { Image, View, Text, StyleSheet } from 'react-native'; 
 import * as Location from 'expo-location';
 
 const Home = () => {
-
+    let [loading, setLoading] = useState(true); 
+    let [weatherDataFlag, setWeatherDataFlag] = useState(false); 
+    const [city, setCity] = useState(""); 
     const [address, setAddress] = useState(); 
     const [weatherData, setWeatherData] = useState(); 
-    
+
+    const getCityLocation = async () => {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+
+        if (status !== 'granted') {
+            console.log("Please grant location permissions");
+            return;
+        }
+
+        let currentLocation = await Location.getCurrentPositionAsync({});
+        const googleGeocodeAPI = 'https://google-maps-geocoding.p.rapidapi.com/geocode/json';
+        
+        try {
+            const response = await fetch(`${googleGeocodeAPI}?latlng=${currentLocation.coords.latitude},${currentLocation.coords.longitude}&types=(cities)&language=en`, {
+              method: 'GET',
+              headers: {
+                'X-RapidAPI-Key': '4edb1629a6msh5e88b54594bb4bfp1882d0jsn9db89acc20de',
+                'X-RapidAPI-Host': 'google-maps-geocoding.p.rapidapi.com',
+              }
+            });
+
+            if (response.ok) {
+              const data = await response.json();
+              setCity(data.results[0].address_components[3].long_name); 
+              if (city !== undefined) {
+                setLoading(false); 
+              }
+            } else {
+              console.error('Failed to fetch data');
+            }
+          } catch (error) {
+            console.error(error);
+          }
+    };
+
+    const getWeatherInfo = async () => {
+        try {
+            console.log(city);
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=1ea1024f7bfa9bdeb4698e2cbefa3060`, {
+                method: 'GET'
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                console.log(data);
+                setWeatherData(data);
+            } else {
+                console.error('Failed to fetch weather data');
+            }
+        } catch (error) {
+            console.error(error); 
+        }
+    }
 
     useEffect(() => {
-        const getCityLocation = async () => {
-            let { status } = await Location.requestForegroundPermissionsAsync();
-
-            if (status !== 'granted') {
-                console.log("Please grant location permissions");
-                return;
-            }
-
-            let currentLocation = await Location.getCurrentPositionAsync({});
-            const googleGeocodeAPI = 'https://google-maps-geocoding.p.rapidapi.com/geocode/json';
-            
-            try {
-                const response = await fetch(`${googleGeocodeAPI}?latlng=${currentLocation.coords.latitude},${currentLocation.coords.longitude}&types=(cities)&language=en`, {
-                  method: 'GET',
-                  headers: {
-                    'X-RapidAPI-Key': '4edb1629a6msh5e88b54594bb4bfp1882d0jsn9db89acc20de',
-                    'X-RapidAPI-Host': 'google-maps-geocoding.p.rapidapi.com',
-                  }
-                });
-
-                if (response.ok) {
-                  const data = await response.json();
-
-                  // fetch weather data based on city
-                  try {
-                    
-                    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${data.results[0].address_components[3].long_name}&appid=1ea1024f7bfa9bdeb4698e2cbefa3060`, {
-                        method: 'GET'
-                    });
-    
-                    if (response.ok) {
-                        const data = await response.json();
-                        console.log(data);
-                        setWeatherData(data);
-                    } else {
-                        console.error('Failed to fetch weather data');
-                    }
-                    } catch (error) {
-                        console.error(error); 
-                    }
-                } else {
-                  console.error('Failed to fetch data');
-                }
-              } catch (error) {
-                console.error(error);
-              }
-        };
-
-        
-   
         getCityLocation(); 
-
-        
-    }, []);
+        if (!loading) {
+            getWeatherInfo();   
+        }
+     
+    }, [loading]);
 
 
 
    
     return (
         <View style={styles.container}>
-            <TextInput placeholder='Address' value={address} onChangeText={setAddress}/>
-
-            <Text>{city}</Text>
+         {!loading ? (
+            <View>
+                {weatherData && (
+                    <View>
+                        <Image 
+                        source={{ uri: `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png` }}
+                        />
+                        <Text>{weatherData.name}, {weatherData.sys.country}</Text>
+                        <Text>Temperature: {Math.round(weatherData.main.temp - 273.15)}°C</Text>
+                    </View>
+                )}
+            </View>
+         ) : (
+            <Text>Loading...</Text>
+         )}
+ 
         </View>
     )
 }
